b264db940b5efd73e5767c18ff5d3955e8613209
Varun Mantri <39723815+varunrmantri23@users.noreply.github.com>
2024-04-21 19:36:18 +0000

    Revert "Merge pull request #5 from varunrmantri23/varunrmantri23-patch-3-1"
    This reverts commit e6432d7c45cb756769e554562398a27da0e24d29, reversing
    changes made to 49f330ac0b3e7f5c8d9d15480bd4c2141404ad00.

--- components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as UserAuthInput } from "./UserAuthInput";
-export { default as Spinner } from "./Spinner";
-export { default as UserProfileDetails } from "./UserProfileDetails";
-export { default as Alert } from "./Alert";
-
